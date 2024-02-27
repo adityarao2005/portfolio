4fe24e2d126c1f115242078d63ac28a27a61d42e
Aditya Rao <adityarao2005@gmail.com>
2024-02-26 23:51:43 -0500

    officially finished backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -10,30 +10,19 @@ import dotenv from "dotenv";
 import router_helper from "@/routes/api-routes";
 import { InsertOnlyFactory, ReadOnlyFactory } from "@/middleware/security";
 import { ArtworkStore, MessageStore, ProjectStore } from "@/models/datastore";
-import multer from "multer";
 //For env File
 dotenv.config();
 import "@/db/config";
-import { uploadImage } from "@/helpers/helpers";
-import imageRoutes from "@/routes/image-routes";
 
 //Express
 const app: Application = express();
 const port = process.env.PORT || 8000;
-
-const multerMid = multer({
-	storage: multer.memoryStorage(),
-	limits: {
-		fileSize: 10 * 1024 * 1024,
-	},
-});
 
 //DataStore
 
 //Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(multerMid.single("file"));
 app.disable("x-powered-by");
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
@@ -45,7 +34,6 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 	next();
 });
 
-app.use(ReadOnlyFactory("/api/upload-image", imageRoutes));
 app.use(ReadOnlyFactory("/api/projects", router_helper(ProjectStore)));
 app.use(ReadOnlyFactory("/api/artworks", router_helper(ArtworkStore)));
 app.use(InsertOnlyFactory("/api/messages", router_helper(MessageStore)));
